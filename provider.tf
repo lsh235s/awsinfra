84ea5ff701d7fe5e53347411023001311d8e86d6
vega2krookies5 <vega2krookies5@gmail.com>
2026-04-30 09:50:14 +0900

    lock remove

--- provider.tf
+++ provider.tf
@@ -7,11 +7,10 @@ terraform {
   }
 
   backend "s3" {
-    bucket         = "myapp-terraform-state-20260429"
+    bucket         = "myapp-terraform-state-20260429-lsh"
     key            = "training/lab06/terraform.tfstate"
     region         = "ap-northeast-2"
     encrypt        = true
-    dynamodb_table = "terraform-lock"
   }
 }
 
