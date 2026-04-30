9830f95ba829f288f33e15ff6d6227d49cdfad40
vega2krookies5 <vega2krookies5@gmail.com>
2026-04-30 09:52:41 +0900

    provider

--- provider.tf
+++ provider.tf
@@ -5,13 +5,6 @@ terraform {
       version = "~> 5.0"
     }
   }
-
-  backend "s3" {
-    bucket         = "myapp-terraform-state-20260429"
-    key            = "training/lab06/terraform.tfstate"
-    region         = "ap-northeast-2"
-    encrypt        = true
-  }
 }
 
 provider "aws" {
